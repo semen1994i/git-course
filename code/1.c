3504338836549293cd5df24287d1f6e11ab19271
semen1994i <urxednay.9@gmail.com>
2022-11-21 14:24:26 +0300

    qwe

---
+++ code/1.c
@@ -0,0 +1,20 @@
+/**
+ * 1.c - запуск в дочернем процессе другой программы
+ *
+ * Copyright (c) 2021, Yaskelaynen Simon <yaskelya@cs.petrsu,ru>
+ *
+ * This code is licensed under a MIT-style license
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+    if ()
+    {;
+    }
+    return 0;
+}
